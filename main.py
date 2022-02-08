70697a7a09f29f30c1d2b08405dc4de7656d251d
lukepawlicki <98533140+lukepawlicki@users.noreply.github.com>
2022-02-08 14:58:53 +0000

    Completed exercise

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-mylist 
+mylist = [int(input("number 1")), int(input("number 2")), int(input("number 3")), int(input("number 4"))]
+print(mylist)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+mylist[0] = mylist[3]
+print(mylist)
